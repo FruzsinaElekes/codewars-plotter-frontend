dd019d89267c124de630cd2096b70b90bc5eca9e
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-15 08:36:44 +0100

    Add styling to BarPlot

--- src/components/BarPlot.js
+++ src/components/BarPlot.js
@@ -5,10 +5,22 @@ export default function BarPlot({ plotData }) {
 
     const options = {
         title: {text: plotData.language},
+        theme: 'dark2',
         axisX: {reversed: true, title: "Rank"},
-        axisY: {title: "count"},
+        axisY: {
+            title: "count",
+            interval: 5
+        },
         data: [{
             type: 'bar',
+            indexLabel: "{y}",
+            indexLabelPlacement: "inside",
+            indexLabelBackgroundColor: "rgba(0,0,0,0.5)",
+            indexLabelRadius: 10,
+            indexLabelOrientation: "horizontal",
+            indexLabelFontSize: "20",
+            indexLabelFontWeight: "bold",
+            indexLabelWidth: 150,
             dataPoints: plotData.dataPoints
         }]
     }
