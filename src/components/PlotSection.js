6346041623c01ecd18bdf030005efbef8eb754f4
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-15 08:37:40 +0100

    Fetch plot data conditionally, add area for PlotMenu

--- src/components/PlotSection.js
+++ src/components/PlotSection.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 import BarPlot from './BarPlot';
+import Collapsed from './CollapsedPlot';
+import PlotMenu from './PlotMenu';
 
 export default function PlotSection({ languages }) {
 
@@ -10,15 +13,36 @@ export default function PlotSection({ languages }) {
     const urlList = languages.map(language => `http://localhost:8080/plot/${language}?username=${username}&apiKey=${apiKey}`)
 
     useEffect(() => {
-        urlList.forEach(url => {
-            axios.get(url)
-            .then(resp => setPlotList(prev => [...prev, resp.data]))
-        });
+        if (plotList.length === 0){
+            urlList.forEach(url => {
+                axios.get(url)
+                .then(resp => setPlotList(prev => [...prev, resp.data]))
+            });
+        }
     }, [])
 
     return (
-        <div>
-            {plotList.map(p => <BarPlot key = {p.language} plotData = {p}/>)}
-        </div>
+        <PlotContainer>
+            <PlotGrid>
+                {plotList.map(p => <BarPlot key = {p.language} plotData = {p}/>)}
+            </PlotGrid>
+            {/* <Collapsed plotList={plotList}></Collapsed> */}
+            <PlotMenu />
+        </PlotContainer>
     )
 }
+
+
+const PlotGrid = styled.div`
+    margin:auto;
+    display: grid;
+    gap: 2em;
+    grid-template-columns: 1fr 1fr;
+    width: 90%;
+`
+
+const PlotContainer = styled.div`
+    margin: auto;
+    width: 60vw;
+    display:flex
+`
