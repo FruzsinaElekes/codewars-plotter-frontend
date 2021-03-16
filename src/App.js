8b7a5afb4516633cba54e1ab4e1f147eb0107d4a
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-16 12:02:47 +0100

    Use routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Stats from './components/Stats'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage';
+import Navbar from './components/Navbar';
+import Registration from './components/Registration';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Stats/>
+      <Router>
+        <Route path="/" component={Navbar}/>
+        <Route exact path="/" component={LandingPage}/>
+        <Route path="/stats" component={Stats} />
+        <Route path="/login-page" component={Login} />
+        <Route path="/registration-page" component={Registration} />
+      </Router>
     </div>
   );
 }
