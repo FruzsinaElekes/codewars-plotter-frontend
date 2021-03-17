103673294600a27b1dec8d7947adfed3305e073e
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-17 11:23:15 +0100

    Add login form, redirect to stats-page if successful

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import {Redirect} from 'react-router-dom'
+import axios from 'axios';
+import styled from 'styled-components'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 export default function Login() {
+
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const [toRedirect, setToRedirect] = useState(false)
+
+    const url = process.env.REACT_APP_ORIGIN + process.env.REACT_APP_LOGIN
+
+    const sendLogin = () => {
+        axios({
+            method: 'post',
+            url: url,
+            data: {
+                username: usernameRef.current.value,
+                password: passwordRef.current.value,
+            },
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(response => setToRedirect(true))
+        .catch(error => {
+            if (error.response.status === 403) alert("Incorrect username or password!")
+        })
+    }
+
     return (
-        <div>
-            login
-        </div>
+        <React.Fragment>
+            {toRedirect 
+            ? <Redirect to="/stats-page" />
+            : <RegFormContainer>
+                <div>
+                    <TextField inputRef={usernameRef} label="Codewars username"></TextField>
+                    <TextField inputRef={passwordRef} type="password" label="Password"></TextField>
+                </div>
+                <div>
+                    <Button onClick={sendLogin}>Login</Button>
+                </div>
+            </RegFormContainer>}
+        </React.Fragment>
     )
 }
+
+const RegFormContainer = styled.div`
+    width: 300px;
+    margin: auto;
+`
