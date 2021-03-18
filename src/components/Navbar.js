345edb319a0a101f7dbf8cdd6187ce8228d53282
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-18 17:41:35 +0100

    Take Navbar off if user context is empty

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
+import { UserContext } from './userAuth/UserContext'
 
 export default function Navbar() {
+
+    const [user, setUser] = useContext(UserContext)
+    const [isVisible, setIsVisible] = useState(Object.keys(user).length !== 0)
+    const deleteUser = () => setUser({})
+    
+    useEffect(() => {
+        setIsVisible(Object.keys(user).length !== 0)
+    }, [user])
+
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/login-page">Login</Link>
-            <Link to="/registration-page">Register</Link>
+            {isVisible && <React.Fragment>
+                <Link onClick={deleteUser} to="/">Log out</Link>
+                <Link to="/stats-page">Statistics</Link>
+                <Link to="/kata-finder">Kata finder</Link>
+            </React.Fragment>
+            }
         </div>
     )
 }
