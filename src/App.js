c82f82aeb228002b7e4c79d92ff06814f165bc9f
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-18 17:40:58 +0100

    Add route for KataFinder to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Stats from './components/statistics/Stats'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Stats from './components/statistics/Stats';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import LandingPage from './components/userAuth/LandingPage';
 import Navbar from './components/Navbar';
-import { UserProvider } from './components/userAuth/UserContext'
+import KataFinder from './components/katafinder/KataFinder';
+import { UserProvider } from './components/userAuth/UserContext';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" component={Navbar}/>
           <Route exact path="/" component={LandingPage}/>
           <Route path="/stats-page" component={Stats} />
+          <Route path="/kata-finder" component={KataFinder} />
         </Router>
       </UserProvider>
     </div>
