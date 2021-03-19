8d6dec417fd8e78625816057282adf02eaba65a1
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-19 12:59:23 +0100

    Add possibility to sort based on language & rank

--- src/components/katafinder/KataFinder.js
+++ src/components/katafinder/KataFinder.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../userAuth/UserContext';
 import KataDescription from './KataDescription';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
 export default function KataFinder() {
 
@@ -9,9 +13,24 @@ export default function KataFinder() {
     const [allKatas, setAllKatas] = useState([])
     const [filtered, setFiltered] = useState([])
 
+    const [language, setLanguage] = useState("none")
+    const languageOptions = [...user.languageRanks.map(r => r.language), "none"]
+    const handleLanguageChange = (event) => setLanguage(event.target.value)
+
+    const [rank, setRank] = useState("none")
+    const rankOptions = ["none", "1 kyu", "2 kyu", "3 kyu", "4 kyu", "5 kyu", "6 kyu", "7 kyu", "8 kyu"]
+    const handleRankChange = (event) => setRank(event.target.value)
+
     useEffect(() => {
         fetchAllKatas()
     }, [])
+
+    useEffect(() => {
+        let filteredList = allKatas;
+        if (language !== "none") filteredList = filteredList.filter(kata => kata.completedLanguages.includes(language))
+        if (rank !== "none") filteredList = filteredList.filter(kata => kata.rank === rank)
+        setFiltered(filteredList)
+    }, [language, rank])
 
     const fetchAllKatas = () => {
         const url = `http://localhost:8080/users/${user.username}/katas`
@@ -19,17 +38,34 @@ export default function KataFinder() {
             .then(response => {
                 setAllKatas(response.data)
                 setFiltered(response.data)
-                console.log(response.data)
             })
             .catch(error => alert("Username does not exist"))
     }
 
     return (
         <div>
-            {filtered !== [] 
-            ? filtered.map(k => <KataDescription key={k.codewarsId} kata = {k}/>)    
-            : "Loading data"
-        }
+            <div>
+                <FormControl>
+                    <InputLabel>Language</InputLabel>
+                    <Select value={language} onChange={handleLanguageChange}>
+                        {languageOptions.map(o => <MenuItem value={o}>{o}</MenuItem>)}
+                    </Select>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Rank</InputLabel>
+                    <Select value={rank} onChange={handleRankChange}>
+                        {rankOptions.map(o => <MenuItem value={o}>{o}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            
+            </div>
+            <div>
+                {filtered !== [] 
+                ? filtered.map(k => <KataDescription key={k.codewarsId} kata = {k}/>)    
+                : "Loading data"
+            }
+            </div>
         </div>
+
     )
 }
