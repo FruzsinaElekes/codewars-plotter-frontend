e72ed3e190d7f817591e0216fec43891190db562
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-25 11:53:51 +0100

    Move filtering logic to Menu component

--- src/components/katafinder/KataFinder.js
+++ src/components/katafinder/KataFinder.js
@@ -2,35 +2,19 @@ import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../userAuth/UserContext';
 import KataDescription from './KataDescription';
-import Button from '@material-ui/core/Button';
 import styled from 'styled-components'
-import { byLanguages, byRank, byTitle } from '../../util/filters'
-import SelectPanel from './SelectPanel'
-import { TextField } from '@material-ui/core';
+import Menu from './Menu'
 
 export default function KataFinder() {
-    const filters = [byLanguages, byRank, byTitle]
+    
     const userSummary = useContext(UserContext)[0]
     const [userCompleted, setUserCompleted] = useContext(UserContext).slice(4,6)
     const [filtered, setFiltered] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const [filterState, setFilterState] = useState({"rank": [], "languages": [], "title": []})
-
-    const languageOptions = userSummary.languageRanks.map(r => r.language)
-    const rankOptions = ["1 kyu", "2 kyu", "3 kyu", "4 kyu", "5 kyu", "6 kyu", "7 kyu", "8 kyu"]
-    const handleFilterChange = (event) => setFilterState(prev => ({ ...prev, [event.target.name]: event.target.value}))
-    const handleTitleFilterChange = (event) => setFilterState(prev => ({ ...prev, "title": event.target.value}))
-    const resetFilterState = () => setFilterState({"rank": [], "languages": [], "title": []})
-    const deleteFilter = (which) => setFilterState(prev => ({...prev, [which]: []}))
 
     useEffect(() => {
         if (userCompleted.length === 0) fetchAllKatas()
     }, [])
-
-    useEffect(() => {
-        let filteredList = userCompleted.filter(kata => filters.every(f => f(kata, filterState)))
-        setFiltered(filteredList)
-    }, [filterState])
 
     const fetchAllKatas = () => {
         const url = `http://localhost:8080/users/${userSummary.username}/katas`
@@ -45,23 +29,7 @@ export default function KataFinder() {
 
     return (
         <FinderContainer>
-            <FilterMenu>
-                <TextField label="title" value={filterState.title} onChange={handleTitleFilterChange} />
-                <SelectPanel type="languages" 
-                            opts ={languageOptions} 
-                            filterState={filterState} 
-                            filterChange={handleFilterChange}
-                            deleteFilter={deleteFilter}
-                            multiple="true"></SelectPanel>
-                <SelectPanel type="rank" 
-                            opts ={rankOptions} 
-                            filterState={filterState} 
-                            filterChange={handleFilterChange}
-                            deleteFilter={deleteFilter}
-                            multiple="false"></SelectPanel>
-                <Button onClick={resetFilterState}>Reset all</Button>
-                <p>Number of items found: {isLoading ? "loading data" : filtered.length}</p>
-            </FilterMenu>
+            <Menu isLoading={isLoading} filteredLength={filtered.length} setFiltered={setFiltered}></Menu>
             <KataList>
                 {filtered.length !== 0
                 ? filtered.map(k => <KataDescription key={k.codewarsId} kata = {k}/>)    
@@ -77,13 +45,6 @@ const FinderContainer = styled.div`
     display:flex;
     margin: 4em auto;
 `
-const FilterMenu = styled.div`
-    padding-left: 2em;
-    width: 20%;
-    display: flex;
-    flex-direction: column;
-`
-
 const KataList = styled.div`
     width: 100%
 `
---
+++ src/components/katafinder/Menu.js
@@ -0,0 +1,58 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import SelectPanel from './SelectPanel'
+import styled from 'styled-components'
+import { UserContext } from '../userAuth/UserContext';
+import { byLanguages, byRank, byTitle } from '../../util/filters'
+
+
+
+export default function Menu({isLoading, filteredLength, setFiltered}) {
+
+    const userSummary = useContext(UserContext)[0]
+    const userCompleted = useContext(UserContext)[4]
+    const [filterState, setFilterState] = useState({"rank": [], "languages": [], "title": []})
+
+    const languageOptions = userSummary.languageRanks.map(r => r.language)
+    const rankOptions = ["1 kyu", "2 kyu", "3 kyu", "4 kyu", "5 kyu", "6 kyu", "7 kyu", "8 kyu"]
+    const filters = [byLanguages, byRank, byTitle]
+    
+    const handleFilterChange = (event) => setFilterState(prev => ({ ...prev, [event.target.name]: event.target.value}))
+    const handleTitleFilterChange = (event) => setFilterState(prev => ({ ...prev, "title": event.target.value}))
+    const resetFilterState = () => setFilterState({"rank": [], "languages": [], "title": []})
+    const deleteFilter = (which) => setFilterState(prev => ({...prev, [which]: []}))
+
+    useEffect(() => {
+        let filteredList = userCompleted.filter(kata => filters.every(f => f(kata, filterState)))
+        setFiltered(filteredList)
+    }, [filterState])
+
+    return (
+        <FilterMenu>
+                <TextField label="title" value={filterState.title} onChange={handleTitleFilterChange} />
+                <SelectPanel type="languages" 
+                            opts ={languageOptions} 
+                            filterState={filterState} 
+                            filterChange={handleFilterChange}
+                            deleteFilter={deleteFilter}
+                            multiple="true"></SelectPanel>
+                <SelectPanel type="rank" 
+                            opts ={rankOptions} 
+                            filterState={filterState} 
+                            filterChange={handleFilterChange}
+                            deleteFilter={deleteFilter}
+                            multiple="false"></SelectPanel>
+                <Button onClick={resetFilterState}>Reset all</Button>
+                <p>Number of items found: {isLoading ? "loading data" : filteredLength}</p>
+            </FilterMenu>
+    )
+}
+
+
+const FilterMenu = styled.div`
+    padding-left: 2em;
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+`
