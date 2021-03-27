260a0c80eca843cfd69f0a7ec61f3daa12341c3f
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-27 15:41:23 +0100

    Make stats page position relative to navbar

--- src/components/statistics/Stats.js
+++ src/components/statistics/Stats.js
@@ -3,6 +3,7 @@ import PlotSection from './PlotSection';
 import UserOverview from './UserOverview';
 import {UserContext} from '../userAuth/UserContext'
 import { Redirect } from 'react-router';
+import styled from 'styled-components'
 
 export default function Stats() {
 
@@ -12,12 +13,19 @@ export default function Stats() {
     return (
         <React.Fragment>
             {Object.keys(user).length > 0
-            ? <div>
+            ? <StatsContainer>
                 <UserOverview userSummary={user}/>
                 <PlotSection languages={(user.languageRanks).map(lr => lr.language)}/>
-            </div>
+            </StatsContainer>
             : <Redirect to='/'></Redirect>
             }
         </React.Fragment>
     )
 }
+
+const StatsContainer = styled.div`
+    display: flex;
+    position: relative;
+    top: 50px;
+    height: 100%
+`
